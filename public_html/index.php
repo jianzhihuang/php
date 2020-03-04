8e487ce2162a58b73e2b50cd3669b0b7a39a73e6
黃ZHI <p0921442701@gmail.com>
2020-03-05 00:15:01 +0800

    test

--- public_html/index.php
+++ public_html/index.php
@@ -1,12 +1,19 @@
 <?php
-
-    for ($i = 10; $i > 0; $i=$i-1)
-    {
-      echo $i."<br/>";
+      $myArray = array("貓","狗","魚");
 
 
+      foreach ($myArray as $key => $value){
 
-    };
+        echo "位置: $key, 值為: $value <br/>";
+
+      }
+    // for ($i = 10; $i > 0; $i=$i-1)
+    // {
+    //   echo $i."<br/>";
+
+
+
+    // };
 
 
 
