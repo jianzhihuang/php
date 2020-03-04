4b2ddb7830965901a60842090a2467c8249a06da
黃ZHI <p0921442701@gmail.com>
2020-03-04 23:22:08 +0800

    git init

--- public_html/index.php
+++ public_html/index.php
@@ -1,8 +1,15 @@
 <?php
     $myArray =  array("漢堡包","巧克力","咖啡");
     print_r($myArray);
+
     echo "<br/><br/>";
     echo $myArray[5];
-    echo "繼續運行"
+    echo "繼續運行";
+    echo "<br/><br/>";
+
+    $anotherArray[0] = "冰淇淋";
+    $anotherArray[1] = "牛奶";
+
+    print_r($anotherArray);
 
 ?>
