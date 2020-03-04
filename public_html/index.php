0051ff76f42a23fec01093bb6d8b0bec3fb554d8
黃ZHI <p0921442701@gmail.com>
2020-03-04 23:40:34 +0800

    test

--- public_html/index.php
+++ public_html/index.php
@@ -1,45 +1,12 @@
 <?php
-    $myArray =  array("漢堡包","巧克力","咖啡");
-    print_r($myArray);
+    $number = 2;
+    if ($number == 1 ) {
 
-    echo "<br/><br/>";
-    echo $myArray[5];
-    echo "繼續運行";
-    echo "<br/><br/>";
-
-    $anotherArray[0] = "冰淇淋";
-    $anotherArray[1] = "牛奶";
-
-    print_r($anotherArray);
-    echo "<br/><br/>";
-    $thirdArray = array(
-
-      "法國" => "法語",
-      "美國" => "英語",
-      "日本" => "日語",
+      echo "True!";
 
 
 
-    );
-    print_r($thirdArray);
-    echo "<br/><br/>";
-    echo $thirdArray["美國"];
-    print_r($thirdArray);
-    echo "<br/><br/>";
-    $anotherArray[] = "沙拉";
-    print_r($anotherArray);
-    echo "<br/><br/>";
-
-    unset($thirdArray["美國"]);
-    print_r($thirdArray);
-    echo "<br/><br/>";
-    $name = "Ken";
-    unset($name);
-    echo $name;
-    
-
-
-
+    } 
 
 
 
