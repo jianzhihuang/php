d4dc24386eb139c75960f8adba57e2f57c9ca99e
黃ZHI <p0921442701@gmail.com>
2020-03-06 00:13:13 +0800

    test

--- public_html/index.php
+++ public_html/index.php
@@ -12,12 +12,33 @@
 <body>
 
         <?php
+
+
+          $name = array("Ken","Josh","Tom");
+          $knowYou = 0 ;
         
           if ($_POST["name"]){
 
 
 
-            echo "你的姓名是 ".$_POST["name"];
+            // echo "你的姓名是 ".$_POST["name"];
+            foreach($name as $name){
+
+              if($_POST["name"] == $name){
+
+
+                  echo "我認識你 , 你是".$_POST["name"];
+                  $knowYou = 1;
+
+              }
+
+            }
+
+            if (!$knowYou) {
+
+              echo "抱歉 , 我不認識你";
+
+            }
 
           }else{
 
