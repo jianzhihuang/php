3c3008424c75abdc806bab1d259afe51e92f8421
黃ZHI <p0921442701@gmail.com>
2020-03-06 23:50:48 +0800

    test

--- public_html/index.php
+++ public_html/index.php
@@ -6,7 +6,7 @@
 
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
-    <script type="text/javascript" src="jquery.min.js"></script>
+    <script src='https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js'></script>
     <style type="text/css">
         body {
             background-color: #F2F3F5;
