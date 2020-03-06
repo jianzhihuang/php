d3423e88a74b1c701e87abc74a3305b788a4acc6
黃ZHI <p0921442701@gmail.com>
2020-03-07 00:07:01 +0800

    test

---
+++ public_html/mail.php
@@ -0,0 +1,18 @@
+<?php
+
+    if (isset($_POST["submit"])) {
+
+    $name = $_POST["name"];
+    $email = $_POST["email"];  
+    $pass1 = $_POST["pass1"];
+    $pass2 = $_POST["pass2"];
+    $message = $_POST["message"];
+}
+
+
+
+
+
+
+
+?>
