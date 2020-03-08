5a321b89fe325d4cf6efcf5902b5777a0e0cbaef
黃ZHI <p0921442701@gmail.com>
2020-03-09 01:03:55 +0800

    test

--- public_html/scraper.php
+++ public_html/scraper.php
@@ -4,7 +4,7 @@
 
     $city = str_replace(" ","-",$city);
     
-    $contents = @file_get_contents("https://www.myweather2.com/City-Town/United-Kingdom/City-of-Westminster/London/14-Day-Forecast.aspx");
+    echo @file_get_contents("https://www.myweather2.com/City-Town/United-Kingdom/City-of-Westminster/London/14-Day-Forecast.aspx");
 
 
 
