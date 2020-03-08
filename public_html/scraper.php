f415b88a1d2c1426382fc665743988873941257f
黃ZHI <p0921442701@gmail.com>
2020-03-09 00:56:26 +0800

    test

--- public_html/scraper.php
+++ public_html/scraper.php
@@ -6,7 +6,7 @@
     
     $contents = @file_get_contents("https://www.myweather2.com/City-Town/United-Kingdom/City-of-Westminster/London/14-Day-Forecast.aspx");
 
-    preg_match("/\"<br><p>\"(.*?)<\/p>/i",$contents, $matches);
+    preg_match("/<br><p>(.*?)<\/p>/i",$contents, $matches);
 
     echo $matches[1];
 
