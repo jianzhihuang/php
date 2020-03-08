cf79c0046941c6b2db29eedb4ca34ad9bf5f72d1
黃ZHI <p0921442701@gmail.com>
2020-03-08 23:44:27 +0800

    test

--- public_html/scraper.php
+++ public_html/scraper.php
@@ -1,9 +1,9 @@
 <?php
 
 
-    echo @file_get_contents("https://www.weather-forecast.com/locations/London/forecasts/latest");
+    $contents = @file_get_contents("https://www.msn.com/zh-tw/weather");
 
-    preg_match("/Light rain/i",$contents, $matches);
+    preg_match("/今天(.*?)天氣/i",$contents, $matches);
 
     print_r($matches);
 
