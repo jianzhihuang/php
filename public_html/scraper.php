41c3069add078d8d756e4129a4d36a1d3409a5fd
黃ZHI <p0921442701@gmail.com>
2020-03-09 01:07:27 +0800

    test

--- public_html/scraper.php
+++ public_html/scraper.php
@@ -4,11 +4,11 @@
 
     $city = str_replace(" ","-",$city);
     
-    echo @file_get_contents("https://www.myweather2.com/City-Town/United-Kingdom/City-of-Westminster/London/14-Day-Forecast.aspx");
+    $contents = @file_get_contents("https://www.myweather2.com/City-Town/United-Kingdom/City-of-Westminster/London/14-Day-Forecast.aspx");
 
 
 
-    preg_match("/Taking a look (.*?) moderate./i",$contents, $matches);
+    preg_match("/<strong>L (.*?) <\strong>/i",$contents, $matches);
 
     print_r($matches);
     echo $matches[1];
