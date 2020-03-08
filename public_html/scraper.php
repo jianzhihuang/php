eb7a018dfae552451277add4b4a470db4b0e643a
黃ZHI <p0921442701@gmail.com>
2020-03-08 20:52:27 +0800

    test

--- public_html/scraper.php
+++ public_html/scraper.php
@@ -1,7 +1,7 @@
 <?php
 
 
-    echo  @file_get_contents("https://www.msn.com/zh-tw/weather");
+    $contents =  @file_get_contents("https://www.msn.com/zh-tw/weather/today/%e5%80%ab%e6%95%a6%e8%8b%b1%e6%a0%bc%e8%98%ad%e8%8b%b1%e5%9c%8b/we-city?q=%e5%80%ab%e6%95%a6-%e8%8b%b1%e6%a0%bc%e8%98%ad&form=PRWLAS&iso=GB&el=3iIt2X8KYBP4L%2bodva3OeQ%3d%3d");
 
     // preg_match("/Light rain/i",$contents, $matches);
 
