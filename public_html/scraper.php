6d2c2798c4593d76968e1adad3ab88f0f228ba2a
黃ZHI <p0921442701@gmail.com>
2020-03-08 23:45:11 +0800

    test

--- public_html/scraper.php
+++ public_html/scraper.php
@@ -3,7 +3,7 @@
 
     $contents = @file_get_contents("https://www.msn.com/zh-tw/weather");
 
-    preg_match("/今天(.*?)天氣/i",$contents, $matches);
+    preg_match("/今明(.*?)天氣穩定/i",$contents, $matches);
 
     print_r($matches);
 
