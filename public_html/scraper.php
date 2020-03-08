d4013e3b89add9aee3addcafcf85719a95a6d174
黃ZHI <p0921442701@gmail.com>
2020-03-08 20:39:28 +0800

    test

--- public_html/scraper.php
+++ public_html/scraper.php
@@ -1,9 +1,11 @@
 <?php
 
 
-    echo @file_get_contents("https://www.weather-forecast.com/locations/London/forecasts/latest");
+    $contents = @file_get_contents("https://www.weather-forecast.com/locations/London/forecasts/latest");
 
+    preg_match("/Light rain (total 7mm)/i",$contents, $matches);
 
+    print_r($matches);
 
 
 ?>
