948dd69148abd09f9cecd3718bda7dce3777b474
黃ZHI <p0921442701@gmail.com>
2020-03-08 20:46:33 +0800

    test

--- public_html/scraper.php
+++ public_html/scraper.php
@@ -1,7 +1,7 @@
 <?php
 
 
-    echo  @file_get_contents("https://www.cwb.gov.tw/V8/C/W/W50_index.html");
+    echo  @file_get_contents("https://www.accuweather.com/en/tw/taipei-city/315078/weather-forecast/315078");
 
     // preg_match("/Light rain/i",$contents, $matches);
 
