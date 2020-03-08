75aa0d335d88c99b61b4e8a89376de7411bf889a
黃ZHI <p0921442701@gmail.com>
2020-03-09 00:42:21 +0800

    test

--- public_html/scraper.php
+++ public_html/scraper.php
@@ -4,7 +4,7 @@
 
     $city = str_replace(" ","-",$city);
     
-    $contents = @file_get_contents("https://zh.weather-forecast.com/locations/".$city."/forecasts/latest");
+    echo  @file_get_contents("https://zh.weather-forecast.com/locations/Uk/forecasts/latest");
 
     preg_match("/\"phrase\">(.*?)<\/span>/i",$contents, $matches);
 
