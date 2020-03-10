6c187c0dc369bfdb5823006a379394fc73a88a25
黃ZHI <p0921442701@gmail.com>
2020-03-11 00:05:21 +0800

    test

--- public_html/index.php
+++ public_html/index.php
@@ -2,8 +2,19 @@
 
     setcookie("id","test",time() + 60 * 60);
 
-    echo $_COOKIE["id"];
+    // echo $_COOKIE["id"];
 
+    if (count($_COOKIE) > 0){
+
+        echo "Cookies已經設置";
+
+
+    }else{
+
+        echo "Cookies沒有被設置";
+
+
+    };
 
 
 ?>
