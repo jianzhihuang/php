7c820d6e5e6594c21394296abb2efeeed5cb4828
黃ZHI <p0921442701@gmail.com>
2020-03-11 00:00:21 +0800

    test

--- public_html/myName.php
+++ public_html/myName.php
@@ -1,16 +1,5 @@
 <?php
 
-    session_start();
-
-    print_r($_SESSION["name"]);
-
-    
-    $_SESSION["name"] = "John";
-
-    session_unset();
-    session_destroy();
-
-    echo "我清除SESSION變量後的值是:".$_SESSION["name"].".";
-
+    echo $_COOKIE["id"];
 
 ?>
