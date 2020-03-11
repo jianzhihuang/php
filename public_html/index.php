10c10a2df6db77ace2160f8edc898c9d12db7764
黃ZHI <p0921442701@gmail.com>
2020-03-11 23:19:43 +0800

    test

--- public_html/index.php
+++ public_html/index.php
@@ -1,14 +1,31 @@
 <?php
 
+    if ($_POST['submit']){
+
+        if(!$_POST['email']) $error.="<br />Please enter your email.";
+        elseif (!filter_var($_POST['email'],FILTER_VALIDATE_EMAIL))$error.="<br />Please a valid email address.";
+
+
+        if(!$_POST['password']) $error.="<br />Please enter your password.";
+        else{
+
+            if (strlen($_POST['password'])<8) $error.="<br />Please enter your a password at least 8 characters.";
+            if (preg_match('`[A-Z]`',$_POST['password']))$error.="<br />Please include at least one capital letter in your password";
+
+        }
+
+        if  ($error) echo "<br />There were error(s) in your signup details:".$error;
+
+    };
 
 ?>
 
 
-<form action="">
+<form method="$_POST" action="">
 
     <input type="email" name="email" id="email" />
     <input type="password" name="password" id="password" />
-    <input type="submit" value="Sign Up"/>
+    <input type="submit" name="submit" value="Sign Up"/>
 
 
 
