f831646332e4f53844a9355b3994d4ee05960a6e
黃ZHI <p0921442701@gmail.com>
2020-03-15 16:19:39 +0800

    test

--- public_html/index.php
+++ public_html/index.php
@@ -131,24 +131,3 @@
     </body>
 
 </html>
-
-<?php include("login.php");?>
-<form method="post" action="">
-
-    <input type="email" name="email" id="email" value="<?php echo addslashes($_POST['email']);?>"/>
-    <input type="password" name="password" id="password" value="<?php echo addslashes($_POST['password']);?>"/>
-    <input type="submit" name="submit" value="Sign Up"/>
-
-
-
-</form>
-
-<form method="post" action="">
-
-    <input type="email" name="loginemail" id="email" value="<?php echo addslashes($_POST['email']);?>"/>
-    <input type="password" name="loginpassword" id="loginpassword" value="<?php echo addslashes($_POST['password']);?>"/>
-    <input type="submit" name="submit" value="Log In"/>
-
-
-
-</form>
