5579ac31700f48dff8a7e9721f7f4f600cefb177
黃ZHI <p0921442701@gmail.com>
2020-03-15 16:06:09 +0800

    test

--- public_html/index.php
+++ public_html/index.php
@@ -58,7 +58,7 @@
 
             <p class="lead text-light">Your own private diary , with you wherever you go.</p>
 
-            <strong class="text-warning">Interested? Sign Up Below!</strong>
+            <p><strong class="text-warning">Interested? Sign Up Below!</strong></p>
 
             <form method="post" action=""></form>
 
