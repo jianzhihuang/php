d253f1378ad11931042ff146971bada99f1b7e9a
黃ZHI <p0921442701@gmail.com>
2020-03-15 23:53:56 +0800

    test

--- public_html/mainpage.php
+++ public_html/mainpage.php
@@ -71,8 +71,8 @@
         </nav>
 
 
-        <div class="jumbotron heroImage text-center">
-            
+        <!-- <div class="jumbotron heroImage text-center">
+             -->
 
 
         <div class="row">
@@ -90,7 +90,7 @@
                 </textarea>
             
         </div> -->
-        </div>
+        <!-- </div> -->
         <div class="container text-center my-5">
             <h1 class="display-4">優 點</h1>
             <p class="lead">為什麼這款軟體如此與眾不同</p>
