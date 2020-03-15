62bf2594e1c18a4c5d033f228e1e97b6df079011
黃ZHI <p0921442701@gmail.com>
2020-03-15 15:14:39 +0800

    test

--- public_html/index.php
+++ public_html/index.php
@@ -1,3 +1,143 @@
+<!DOCTYPE html>
+<html lang="zh-Hant-TW">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+
+    <title>ken-huang.com</title>
+    <style>
+        .heroImage {
+            background-image: url("image/hero1.jpg");
+            height: 550px;
+            border-radius: 0;
+            background-size: cover;
+        }
+        
+        .footer-height {
+            height: 550px;
+            margin-bottom: 0;
+            padding-top: 150px;
+        }
+        
+        #appstoreImg {
+            width: 300px;
+        }
+    </style>
+
+    <body>
+
+        <nav class="navbar navbar-expand-xl navbar-light bg-light">
+
+            <button class="navbar-toggler" data-toggle="collapse" data-target="#navbarCollapse" type="button">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarCollapse">
+
+                <a href="#" class="navbar-brand">Ken-huang.com</a>
+
+                <ul class="navbar-nav mr-auto">
+
+                    <li class="nav-item active">
+                        <a href="#" class="nav-link">主 頁</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="#" class="nav-link">關於我們</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="#" class="nav-link">下載App</a>
+                    </li>
+                </ul>
+
+                <form class="form-inline">
+                    <input class="form-control mr-2" type="email" name="email" placeholder="郵箱地址">
+                    <input class="form-control mr-2" type="password" name="password" placeholder="密碼">
+                    <button type="submit" name="submit" class="btn btn-outline-warning">登入</button>
+                </form>
+
+            </div>
+
+        </nav>
+
+        <div class="jumbotron heroImage text-center">
+            <h1 class="text-light display-4">我的軟體</h1>
+
+            <p class="lead text-light">你可以馬上試用這款軟體</p>
+
+            <p class="lead text-light">如果你想了解更多關於這款軟體的資訊,請<strong class="text-warning">加入我們的討論群!</strong></p>
+
+            <div class="input-group col-md-7 mx-auto">
+                <div class="input-group-prepend">
+                    <span class="input-group-text">@</span>
+
+                </div>
+                <input type="email" name="email" placeholder="請輸入你的郵箱地址" class="form-control">
+            </div>
+            <div class="mt-3">
+                <button type="button" name="submit" class="btn btn-warning btn-lg">發 送</button></div>
+
+        </div>
+        <div class="container text-center my-5">
+            <h1 class="display-4">優 點</h1>
+            <p class="lead">為什麼這款軟體如此與眾不同</p>
+            <div class="row">
+                <div class="col" style="width: 18rem;">
+                    <div class="card">
+                        <img src="image/view.jpg" class="card-img-top">
+                        <div class="card-body">
+                            <h5 class="card-title">標 題</h5>
+                            <p class="card-text">這是一段關於軟體優點的描述,這是一段關於軟體優點的描述,這是一段關於軟體優點的描述,這是一段關於軟體優點的描述,這是一段關於軟體優點的描述,這是一段關於軟體優點的描述,這是一段關於軟體優點的描述,這是一段關於軟體優點的描述,這是一段關於軟體優點的描述,</p>
+                            <a href="#" class="btn btn-warning">了解更多</a>
+
+                        </div>
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="card" style="width: 18rem;">
+                        <img src="image/images.png" class="card-img-top">
+                        <div class="card-body">
+                            <h5 class="card-title">標 題</h5>
+                            <p class="card-text">這是一段關於軟體優點的描述,這是一段關於軟體優點的描述,這是一段關於軟體優點的描述,這是一段關於軟體優點的描述,這是一段關於軟體優點的描述,這是一段關於軟體優點的描述,這是一段關於軟體優點的描述,這是一段關於軟體優點的描述,這是一段關於軟體優點的描述,</p>
+                            <a href="#" class="btn btn-warning">了解更多</a>
+
+                        </div>
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="card " style="width: 18rem;">
+                        <img src="image/3.jpg" class="card-img-top">
+                        <div class="card-body">
+                            <h5 class="card-title">標 題</h5>
+                            <p class="card-text">這是一段關於軟體優點的描述,這是一段關於軟體優點的描述,這是一段關於軟體優點的描述,這是一段關於軟體優點的描述,這是一段關於軟體優點的描述,這是一段關於軟體優點的描述,這是一段關於軟體優點的描述,這是一段關於軟體優點的描述,這是一段關於軟體優點的描述,</p>
+                            <a href="#" class="btn btn-warning">了解更多</a>
+
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+
+        </div>
+
+        <div class="jumbotron text-center footer-height">
+            <h1 class="display-4">下載軟體</h1>
+            <p class="lead">聯繫方式:p0921442701@163.com</p>
+            <img src="image/App_Store_(black).png" id="appstoreImg">
+        </div>
+
+        <!-- Optional JavaScript -->
+        <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+        <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+    </body>
+
+</html>
+
 <?php include("login.php");?>
 <form method="post" action="">
 
