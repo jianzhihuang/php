1a38bd5ad125e4febce6931f7f02c6f8a2d26e41
黃ZHI <p0921442701@gmail.com>
2020-03-15 20:27:21 +0800

    test

--- public_html/updatediary.php
+++ public_html/updatediary.php
@@ -5,7 +5,7 @@
     include("connection.php");
     
     
-    $query = "UPDATE users SET diary='".mysqli_real_escape_string($link,$_POST['diary'])."'WHERE id'".$_SESSION['id']."'LIMIT 1";
+    $query = "UPDATE users SET diary='".mysqli_real_escape_string($link,$_POST['diary'])."'WHERE id='".$_SESSION['id']."'LIMIT 1";
 
     mysqli_query($link,$query);
 
