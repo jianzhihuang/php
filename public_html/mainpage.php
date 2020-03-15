02bc7478076ba987494087371f7c6756c1ff8133
黃ZHI <p0921442701@gmail.com>
2020-03-15 23:39:47 +0800

    test

--- public_html/mainpage.php
+++ public_html/mainpage.php
@@ -1,4 +1,5 @@
-<?php include("login.php");
+<?php include("login.php"); 
+
 	session_start();
 	
 	include("connection.php");
@@ -10,142 +11,155 @@
 	$row = mysqli_fetch_array($result);
 	
 	$diary = $row['diary'];
+
 ?>
 <!DOCTYPE html>
-<html lang="zh-Hant-TW">
-
-<head>
-    <!-- Required meta tags -->
+<html lang="en">
+  <head>
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title>Secret Diary</title>
 
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <!-- Bootstrap -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
 
-    <title>Sectret Diary</title>
-    <style>
-        .heroImage {
-            background-image: url("project/image/hero1.jpg");
-            height: 550px;
-            border-radius: 0;
-            background-size: cover;
-        }
-        
-        .footer-height {
-            height: 550px;
-            margin-bottom: 0;
-            padding-top: 150px;
-        }
-        
-        #appstoreImg {
-            width: 300px;
-        }
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+   
+    <!-- jQuery library -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.2/jquery.min.js"></script>
 
-        #Sectret{
-
-            font-size:1.7rem;
-            color: grey;
-
-        }
-
-    </style>
-
-    <body>
-        <nav class="navbar  navbar-expand-xl navbar-light bg-light">
-            <button class="navbar-toggler" data-toggle="collapse" data-target="#navbarCollapse" type="button">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarCollapse">
-
-                <a href="#" id="Sectret" class="navbar-brand mr-auto font-weight-bold">Sectret Diary</a>
-
-            </div>    
-            <div class="pull-right">
-                    <ul class="navbar-nav nav">
-
-                        <li><a href="">Log Out</a></li>
-
-                    </ul>
-        
-            </div> 
-        </nav>
-
-
-        <div class="jumbotron heroImage text-center">
-            
-        <div class="row contentContainer">
-        
-        <textarea name="" class="form-control" id=""><?php echo $diary;?></textarea>
-            
-        </div>
-        </div>
-        <div class="container text-center my-5">
-            <h1 class="display-4">優 點</h1>
-            <p class="lead">為什麼這款軟體如此與眾不同</p>
-            <div class="row">
-                <div class="col" style="width: 18rem;">
-                    <div class="card">
-                        <img src="project/image/view.jpg" class="card-img-top">
-                        <div class="card-body">
-                            <h5 class="card-title">標 題</h5>
-                            <p class="card-text">這是一段關於軟體優點的描述,這是一段關於軟體優點的描述,這是一段關於軟體優點的描述,這是一段關於軟體優點的描述,這是一段關於軟體優點的描述,這是一段關於軟體優點的描述,這是一段關於軟體優點的描述,這是一段關於軟體優點的描述,這是一段關於軟體優點的描述,</p>
-                            <a href="#" class="btn btn-warning">了解更多</a>
-
-                        </div>
-                    </div>
-                </div>
-                <div class="col">
-                    <div class="card" style="width: 18rem;">
-                        <img src="project/image/images.png" class="card-img-top">
-                        <div class="card-body">
-                            <h5 class="card-title">標 題</h5>
-                            <p class="card-text">這是一段關於軟體優點的描述,這是一段關於軟體優點的描述,這是一段關於軟體優點的描述,這是一段關於軟體優點的描述,這是一段關於軟體優點的描述,這是一段關於軟體優點的描述,這是一段關於軟體優點的描述,這是一段關於軟體優點的描述,這是一段關於軟體優點的描述,</p>
-                            <a href="#" class="btn btn-warning">了解更多</a>
-
-                        </div>
-                    </div>
-                </div>
-                <div class="col">
-                    <div class="card " style="width: 18rem;">
-                        <img src="project/image/3.jpg" class="card-img-top">
-                        <div class="card-body">
-                            <h5 class="card-title">標 題</h5>
-                            <p class="card-text">這是一段關於軟體優點的描述,這是一段關於軟體優點的描述,這是一段關於軟體優點的描述,這是一段關於軟體優點的描述,這是一段關於軟體優點的描述,這是一段關於軟體優點的描述,這是一段關於軟體優點的描述,這是一段關於軟體優點的描述,這是一段關於軟體優點的描述,</p>
-                            <a href="#" class="btn btn-warning">了解更多</a>
-
-                        </div>
-                    </div>
-                </div>
-
-            </div>
-
-        </div>
-
-        <div class="jumbotron text-center footer-height">
-            <h1 class="display-4">下載軟體</h1>
-            <p class="lead">聯繫方式:p0921442701@163.com</p>
-            <img src="project/image/App_Store_(black).png" id="appstoreImg">
-        </div>
-
-        <!-- Optional JavaScript -->
-        <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-        <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
-        <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
-        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous">
-        </script>
-        
-        <script>
-            $(".contentContainer").css("min-height",$(window).height());
-            $("textarea").css("min-height",$(window).height()*0.8);
-            $("textarea").keyup(function() {
-
-            $.post("updatediary.php", {diary:$("textarea").val()});
-
-            });
-
-
-
-        </script>
-    </body>
-
+<!-- Latest compiled JavaScript -->
+<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+	
+	<style>
+	
+		.navbar-brand {
+			font-size:1.8em;
+			font-weight:bold;
+		}
+		
+		#topContainer {
+			background-image:url("background.jpg");
+			height:400px;
+			background-size:cover;
+			width:100%;
+		}
+		
+		#topRow {
+			margin-top:100px;
+			text-align:center;
+		}
+		
+		#topRow h1 {
+		
+			font-size:3em;
+		}
+		
+		.bold {
+			font-weight:bold;
+		
+		}
+		
+		.marginTop {
+			margin-top:30px;
+		
+		}
+		
+		.center {
+			text-align:center;
+		
+		}
+		
+		.title {
+			margin-top:100px;
+			font-size:3em;
+		
+		}
+		
+		#footer {
+			background-color:#E98C6D;
+			width:100%;
+		
+		}
+		
+		.appstoreImage {
+		
+			width:300px;
+		}
+		
+		.marginBottom {
+			margin-bottom:30px;
+		
+		}
+		
+		.white {
+			color:white;
+		
+		}
+	</style>
+	
+  </head>
+  <body>
+  	
+  	<div class="navbar navbar-default navbar-fixed-top">
+  	
+  		<div class="container">
+  		
+  			<div class="navbar-header pull-left">
+  			
+  				<a class="navbar-brand">Secret Diary</a>
+  			
+  			</div>
+  			
+  			<div class="pull-right">
+  				
+  				<ul class="navbar-nav nav pull-right">
+  				
+  					<li><a href="index.php?logout=1">Log Out</a></li>
+  					
+  				</ul>
+  				
+  			</div>
+  		
+  		</div>
+  	
+  	</div>
+  	
+  	<div class="container contentContainer" id="topContainer">
+  	
+  		<div class="row">
+  			
+  			<div class="col-md-6 col-md-offset-3" id="topRow">
+  			
+  				<textarea class="form-control"><?php echo $diary; ?></textarea>
+  			
+  			</div>
+  		
+  		</div>
+  	
+  	</div>
+  	
+  	
+  	
+  	<script>
+  		
+  		$(".contentContainer").css("min-height",$(window).height());
+  		
+  		$("textarea").css("min-height",$(window).height()*0.8);
+  		
+  		$("textarea").keyup(function() {
+  		
+  			$.post("updatediary.php", {diary:$("textarea").val()});
+  			
+  		});
+  		
+  	</script>
+  </body>
 </html>
